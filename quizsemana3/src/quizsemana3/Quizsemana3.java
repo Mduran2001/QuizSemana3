044541bf8d2e26ec0d8c65f01ac0cc013f88b8b5
M.Duran <duranbmario@gmail.com>
2026-01-28 20:50:45 -0600

    Add files via upload

---
+++ quizsemana3/src/quizsemana3/Quizsemana3.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quizsemana3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mario
+ */
+public class Quizsemana3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String lectura;
+        String codigo;
+        String price;        
+        lectura = JOptionPane.showInputDialog(null,"Que articulo desea:");
+        codigo = JOptionPane.showInputDialog(null,"Ingrese el codigo del articuo,TEC,ROP,HOG:");
+        price = JOptionPane.showInputDialog(null,"Ingrese el precio del articulo:");
+        
+        if (1 <=1 ){
+            
+            JOptionPane.showMessageDialog(null, "El valor a pagar es :"+price);
+        }
+        JOptionPane.showMessageDialog(null, "");
+        
+        
+    }
+    
+}
